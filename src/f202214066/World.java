3d199ccf68465526205c50d22f7be2c8bedce4a8
Park, Suhong <tnektnek1212@naver.com>
2025-10-19 18:59:32 +0900

    five

--- src/f202214066/World.java
+++ src/f202214066/World.java
@@ -5,5 +5,9 @@ public class World {
     public static void run() {
         System.out.println("202214066");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
     
 }
